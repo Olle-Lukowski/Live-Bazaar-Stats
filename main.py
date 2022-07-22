46b00e25e087ba2c1c498dd73ef46972580185bc
Olle Lukowski <63189113+Olle-Lukowski@users.noreply.github.com>
2022-07-22 20:49:38 +0200

    Update main.py
    Made it work with pyinstaller

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from ui import TrackerInterface
 from requester import Requester
 from saver import DataSaver
+import sys, os
 
+
+def override_where():
+    """ overrides certifi.core.where to return actual location of cacert.pem"""
+    # change this to match the location of cacert.pem
+    return os.path.abspath("cacert.pem")
+
+
+# is the program compiled?
+if hasattr(sys, "frozen"):
+    import certifi.core
+
+    os.environ["REQUESTS_CA_BUNDLE"] = override_where()
+    certifi.core.where = override_where
+
+    # delay importing until after where() has been replaced
+    import requests.utils
+    import requests.adapters
+    # replace these variables in case these modules were
+    # imported before we replaced certifi.core.where
+    requests.utils.DEFAULT_CA_BUNDLE_PATH = override_where()
+    requests.adapters.DEFAULT_CA_BUNDLE_PATH = override_where()
 data_requester = Requester("https://api.hypixel.net/skyblock/bazaar")
 data_saver = DataSaver("data.json")
 gui = TrackerInterface(data_requester, data_saver)
